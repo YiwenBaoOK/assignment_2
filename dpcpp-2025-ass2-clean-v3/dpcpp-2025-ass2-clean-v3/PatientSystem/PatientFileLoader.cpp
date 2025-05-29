63996b07494971690918123dbcd01538bfd240d1
Yiwen Bao <baoyy005@mymail.unisa.edu.au>
2025-05-29 22:44:18 +1000

    Add Design document
    Adding Design document into the project.

--- dpcpp-2025-ass2-clean-v3/dpcpp-2025-ass2-clean-v3/PatientSystem/PatientFileLoader.cpp
+++ dpcpp-2025-ass2-clean-v3/dpcpp-2025-ass2-clean-v3/PatientSystem/PatientFileLoader.cpp
@@ -14,12 +14,11 @@ using namespace std;
 
 std::vector<Patient*> PatientFileLoader::loadPatientFile(const std::string& file)
 {
-	vector<Patient*> patients{};
+    vector<Patient*> patients{};
 
     std::ifstream inFile(file);
     if (inFile.is_open()) {
         // TODO: load your file here
+        return patients;
     }
-
-    return patients;
 }
